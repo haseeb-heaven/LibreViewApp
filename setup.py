95ee8ad53fb355bfced2ad214666b16328032e98
Haseeb <haseeb_heaven@yahoo.com>
2025-07-24 23:54:48 +0530

    refactor: Update package version and include specific packages in setup

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="libreapp",
-    version="0.1.0",
-    packages=find_packages(),
+    version="1.1.0",
+    packages=find_packages(include=['libreapp', 'libreapp.*']),
     install_requires=[
         "requests>=2.32.4",
         "python-dotenv>=1.1.1"
     ],
+    python_requires=">=3.7",
 )
